1e4057a441c2be30658e37ee0511523e3085a9c9
Wiebke <wiebke.feldt@gmail.com>
2023-03-30 18:42:02 +0200

    individual view

---
+++ client/src/components/Accordion.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+const Accordion = ({ content }) => {
+  const [expanded, setExpanded] = useState(false);
+  const toggleExpanded = () => setExpanded((current) => !current);
+
+  return (
+    <div
+      className="my-2 sm:my-4 md:my-6 shadow-sm cursor-pointer bg-white"
+      onClick={toggleExpanded}
+    >
+      <div className="text-left items-center h-20 select-none flex justify-between flex-row">
+        <h5 className="flex-1 text-xl">Description</h5>
+        <div className="flex-none pl-2">
+          <svg
+            data-accordion-icon
+            class="w-6 h-6 shrink-0"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </div>
+      </div>
+      <div
+        className={`pt-0 overflow-hidden transition-[max-height] duration-500 ease-in ${
+          expanded ? "max-h-40" : "max-h-0"
+        }`}
+      >
+        <p className="pb-4 text-left">{content}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Accordion;
