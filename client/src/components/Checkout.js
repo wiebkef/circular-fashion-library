255ef711dd1c2a029010251cc733909c567c4347
Liliia <liliia.shvorob@oa.edu.ua>
2023-04-04 16:06:26 +0200

    Cart, Checkout added

--- client/src/components/Checkout.js
+++ client/src/components/Checkout.js
@@ -1,11 +1,264 @@
-import React from 'react'
+import { useState } from "react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { Switch } from "@headlessui/react";
+import { Link } from "react-router-dom";
 
-function Checkout() {
-  return (
-    <div>
-      
-    </div>
-  )
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
 }
 
-export default Checkout
+export default function Checkout() {
+  const [agreed, setAgreed] = useState(false);
+
+  return (
+    <div className="bg-white px-6 sm:py-10 lg:px-6 py-14">
+      <div className="py-0 px-10 flex justify-left bg-white">
+        <Link
+          to="/cart"
+          className="bg-gray-500 text-white font-bold py-2 px-4 rounded-lg hover:bg-brand lg:px-10"
+        >
+          Back to Cart
+        </Link>
+      </div>
+      <div className="mx-auto max-w-2xl text-center">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
+          Checkout
+        </h2>
+        <p className="mt-2 text-lg leading-8 text-gray-600">
+          Please fill out the contact and shipping information
+        </p>
+      </div>
+      <form action="#" method="POST" className="mx-auto mt-4 max-w-xl sm:mt-5">
+        <h3 className="pb-4 text-base font-bold tracking-tight text-gray-900 sm:text-lg text-left">
+          CONTACT & SHIPPING INFORMATION
+        </h3>
+        <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+          <div className="sm:col-span-2">
+            <label
+              htmlFor="email"
+              className="block text-sm font-semibold leading-6 text-gray-900 text-left"
+            >
+              Email
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                autoComplete="email"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-brand sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="first-name"
+              className="block text-sm font-semibold leading-6 text-gray-900 text-left"
+            >
+              First name *
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="text"
+                name="first-name"
+                id="first-name"
+                autoComplete="given-name"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-brand sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="last-name"
+              className="block text-sm font-semibold leading-6 text-gray-900 text-left"
+            >
+              Last name *
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="text"
+                name="last-name"
+                id="last-name"
+                autoComplete="family-name"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-brand sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div className="sm:col-span-2">
+            <label
+              htmlFor="adress"
+              className="block text-sm font-semibold leading-6 text-gray-900 text-left"
+            >
+              Street, Number *
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="text"
+                name="adress"
+                id="adress"
+                autoComplete="adress"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-brand sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="first-name"
+              className="block text-sm font-semibold leading-6 text-gray-900 text-left"
+            >
+              City *
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="text"
+                name="first-name"
+                id="first-name"
+                autoComplete="given-name"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-brand sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="country"
+              className="block text-sm font-semibold leading-6 text-gray-900 text-left"
+            >
+              Country *
+            </label>
+            <div className="mt-2.5">
+              <select
+                id="country"
+                name="country"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-brand sm:text-sm sm:leading-6"
+              >
+                <option value="germany">Germany</option>
+                <option value="france">France</option>
+              </select>
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="state"
+              className="block text-sm font-semibold leading-6 text-gray-900 text-left"
+            >
+              State / Province *
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="text"
+                name="city"
+                id="city"
+                autoComplete="city"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-brand sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="pc"
+              className="block text-sm font-semibold leading-6 text-gray-900 text-left"
+            >
+              Postal Code *
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="text"
+                name="pc"
+                id="pc"
+                autoComplete="postal code"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-brand sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div className="sm:col-span-2">
+            <label
+              htmlFor="phone-number"
+              className="block text-sm font-semibold leading-6 text-gray-900 text-left"
+            >
+              Phone number
+            </label>
+            <div className="relative mt-2.5">
+              <div className="absolute inset-y-0 left-0 flex items-center">
+                <label htmlFor="country" className="sr-only">
+                  Country
+                </label>
+                <select
+                  id="country"
+                  name="country"
+                  className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-brand sm:text-sm"
+                >
+                  <option>EU</option>
+                  <option>US</option>
+                </select>
+                <ChevronDownIcon
+                  className="pointer-events-none absolute right-3 top-0 h-full w-5 text-gray-400"
+                  aria-hidden="true"
+                />
+              </div>
+              <input
+                type="tel"
+                name="phone-number"
+                id="phone-number"
+                autoComplete="tel"
+                className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-brand sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div className="sm:col-span-2">
+            <label
+              htmlFor="message"
+              className="block text-sm font-semibold leading-6 text-gray-900 text-left"
+            >
+              Additional Information
+            </label>
+            <div className="mt-2.5">
+              <textarea
+                name="message"
+                id="message"
+                defaultValue="Apartament/Company/Receiver"
+                rows={4}
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-brand sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <Switch.Group as="div" className="flex gap-x-4 sm:col-span-2">
+            <div className="flex h-6 items-center">
+              <Switch
+                checked={agreed}
+                onChange={setAgreed}
+                className={classNames(
+                  agreed ? "bg-brand" : "bg-gray-200",
+                  "flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-brand"
+                )}
+              >
+                <span className="sr-only">Agree to policies</span>
+                <span
+                  aria-hidden="true"
+                  className={classNames(
+                    agreed ? "translate-x-3.5" : "translate-x-0",
+                    "h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out"
+                  )}
+                />
+              </Switch>
+            </div>
+            <Switch.Label className="text-sm leading-6 text-gray-600">
+              By selecting this, you agree to our{" "}
+              <a href="#" className="font-semibold text-brand">
+                privacy&nbsp;policy
+              </a>
+              .
+            </Switch.Label>
+          </Switch.Group>
+        </div>
+        <div className="mt-10">
+          <button
+            type="submit"
+            className="block w-full rounded-md bg-brand px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-brand focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-brand"
+          >
+            Confirm Order
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
